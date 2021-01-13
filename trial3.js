21ee7194de8a97180b0a991f04f637a21a2426b9
FractionMari <lesteberg@gmail.com>
2021-01-13 12:44:09 +0100

    added some more code

--- trial3.js
+++ trial3.js
@@ -59,7 +59,7 @@ let yGravAcc = event.accelerationIncludingGravity.y;
   updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
   updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
 
-  updateFieldIfNotNull('Accelerometer_gx2', event.accelerationIncludingGravity.x, 20);
+  updateFieldIfNotNull('Accelerometer_gx2', event.accelerationIncludingGravity.x, 2000);
 
   //updateFieldIfNotNull('Accelerometer_gy2', event.accelerationIncludingGravity.y);
   //updateFieldIfNotNull('Accelerometer_gz2', event.accelerationIncludingGravity.z);
