ea3223cf6d2fe7eaee7e7db7da7394f5af3d6917
Mari <mari@eduroam-193-157-182-132.wlan.uio.no>
2021-01-19 11:48:05 +0100

    trying something different

---
+++ trial5.js
@@ -0,0 +1,32 @@
+// Prøver med en annen tutorial fra https://w3c.github.io/motion-sensors/
+function updateFieldIfNotNull(fieldName, value, precision=10){
+    if (value != null)
+      document.getElementById(fieldName).innerHTML = value.toFixed(precision);
+  }
+
+  
+class LowPassFilterData {
+    constructor(reading, bias) {
+      Object.assign(this, { x: reading.x, y: reading.y, z: reading.z });
+      this.bias = bias;
+    }
+  
+    update(reading) {
+      this.x = this.x * this.bias + reading.x * (1 - this.bias);
+      this.y = this.y * this.bias + reading.y * (1 - this.bias);
+      this.z = this.z * this.bias + reading.z * (1 - this.bias);
+    }
+  };
+  
+  const accl = new Accelerometer({ frequency: 20 });
+                
+  // Isolate gravity with low-pass filter.
+  const filter = new LowPassFilterData(accl, 0.8);
+  
+  accl.onreading = () => {
+    filter.update(accl); // Pass latest values through filter.
+    updateFieldIfNotNull('test_x', filter.x );
+    console.log(`Isolated gravity (${filter.x}, ${filter.y}, ${filter.z})`);
+  }
+  
+  accl.start();
