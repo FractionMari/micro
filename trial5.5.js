4ca8d0760fab7d0cfd1ebf25402f5e94a508e8d3
Mari <mari@eduroam-193-157-182-132.wlan.uio.no>
2021-01-19 15:46:30 +0100

    trying something different

--- trial5.5.js
+++ trial5.5.js
@@ -24,7 +24,7 @@
     volume.gain.value = newAcc;
     //volume.gain.value = 0.5;
     oscillator.connect(volume); 
-    oscillator2.connect(volume); 
+    //oscillator2.connect(volume); 
     volume.connect(context.destination);  
     
 
