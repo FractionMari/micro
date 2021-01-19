f8b864d2dc4d05f1896bac95594994785460b140
Mari <mari@eduroam-193-157-182-132.wlan.uio.no>
2021-01-19 11:58:02 +0100

    trying something different

--- trial5.js
+++ trial5.js
@@ -1,5 +1,5 @@
 // Prøver med en annen tutorial fra https://w3c.github.io/motion-sensors/
-function updateFieldIfNotNull(fieldName, value, precision=10){
+function updateFieldIfNotNull(fieldName, value, precision=2){
     if (value != null)
       document.getElementById(fieldName).innerHTML = value.toFixed(precision);
   }
@@ -22,12 +22,21 @@ class LowPassFilterData {
                 
   // Isolate gravity with low-pass filter.
   const filter = new LowPassFilterData(accl, 0.8);
+
+  let xValue = filter.x;
+  let yValue = filter.y;
+  let zValue = filter.z;
+
+  let totAcc = Math.sqrt(Math.abs(xValue^2) + Math.abs(yValue^2) + Math.abs(zValue^2));
+
+
   
   accl.onreading = () => {
     filter.update(accl); // Pass latest values through filter.
     updateFieldIfNotNull('test_x', filter.x );
     updateFieldIfNotNull('test_y', filter.y );
     updateFieldIfNotNull('test_z', filter.z );
+    updateFieldIfNotNull('total_acc', totAcc );
     console.log(`Isolated gravity (${filter.x}, ${filter.y}, ${filter.z})`);
   }
   
