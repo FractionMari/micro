9b3a4aaa2ab23fe08277b8e1775c37828e38571d
Mari <mari@eduroam-193-157-182-132.wlan.uio.no>
2021-01-20 14:48:21 +0100

    trying with audio files instead

--- trial5.js
+++ trial5.js
@@ -24,9 +24,9 @@ class LowPassFilterData {
     } */
 
     update(reading) {
-      this.x = this.x * this.bias + reading.x * (1 - this.bias);
-      this.y = this.y * this.bias + reading.y * (1 - this.bias);
-      this.z = this.z * this.bias + reading.z * (1 - this.bias);
+      this.x = this.x * this.bias + this.x * (1 - this.bias);
+      this.y = this.y * this.bias + this.y * (1 - this.bias);
+      this.z = this.z * this.bias + this.z * (1 - this.bias);
     }
     
 
