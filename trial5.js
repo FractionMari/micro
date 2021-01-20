5b50873be1c60fe392cf536c76c6adc89ffc6d83
Mari <mari@Maris-MacBook-Pro.local>
2021-01-20 21:08:41 +0100

    minor change to js file

--- trial5.js
+++ trial5.js
@@ -36,13 +36,13 @@ class LowPassFilterData {
   // LowPassFilterData2 experiment
 class LowPassFilterData2 {
     constructor(reading2) {
-      Object.assign(this, { x2: reading2.x2, y2: reading2.y2, z2: reading2.z2 });
+      Object.assign(this, { x: reading2.x, y: reading2.y, z: reading2.z });
     }
 
   update(reading2) {
-    this.x2 = reading2.x2;
-    this.y2 = reading2.y2;
-    this.z2 = reading2.z2;
+    this.x = reading2.x;
+    this.y = reading2.y;
+    this.z = reading2.z;
   }
 
 };
@@ -61,9 +61,9 @@ class LowPassFilterData2 {
     let xValue = filter.x;
     let yValue = filter.y;
     let zValue = filter.z;
-    let xFilter = filter2.x2;
-    let yFilter = filter2.y2;
-    let zFilter = filter2.z2;
+    let xFilter = filter2.x;
+    let yFilter = filter2.y;
+    let zFilter = filter2.z;
   
     let totAcc = Math.sqrt((xValue ** 2) + (yValue ** 2) + (zValue ** 2));
     let totFilter = Math.sqrt((xFilter ** 2) + (yFilter ** 2) + (zFilter ** 2));
@@ -75,13 +75,13 @@ class LowPassFilterData2 {
     filter.update(accl); // Pass latest values through filter.
     filter2.update(filter); // Pass latest values through filter.
     updateFieldIfNotNull('test_x', filter.x );
-    updateFieldIfNotNull('filter_x', filter2.x2 );
+    updateFieldIfNotNull('filter_x', filter2.x );
 
     updateFieldIfNotNull('test_y', filter.y );
-    updateFieldIfNotNull('filter_y', filter2.y2 );
+    updateFieldIfNotNull('filter_y', filter2.y );
 
     updateFieldIfNotNull('test_z', filter.z );
-    updateFieldIfNotNull('filter_z', filter2.z2 );
+    updateFieldIfNotNull('filter_z', filter2.z );
 
     updateFieldIfNotNull('total_acc', totAcc );
     updateFieldIfNotNull('total_filter', totFilter );
