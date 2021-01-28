80e9ed03c83805fa4ef49e988945d84603077b75
Mari <mari@Maris-MacBook-Pro.local>
2021-01-28 12:26:17 +0100

    testing some stuff

--- trial9.js
+++ trial9.js
@@ -161,7 +161,7 @@ class LowPassFilterData {
 
 newAcc = (clamp(0.1, 0.5, newAcc));
 volume.gain.value = newAcc;
-biquadFilter.detune.value = biquadAcc;
+biquadFilter.frequency.value = biquadAcc;
 
 }  
 
