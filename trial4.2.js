1535844cf038e8b95f6c40f07cbf7b56df6d413f
Mari <mari@Maris-MacBook-Pro.local>
2021-01-29 13:41:41 +0100

    testing some stuff

--- trial4.2.js
+++ trial4.2.js
@@ -181,7 +181,7 @@ class LowPassFilterData {
   function clamp(min, max, val) {
     return Math.min(Math.max(min, +val), max);
   }
-  var fn = generateScaleFunction(0, 1.5, 0.5, 0);
+  var fn = generateScaleFunction(0, 13, 0.5, 0);
 
   newAcc = fn(diffAcc);
 
@@ -191,7 +191,7 @@ class LowPassFilterData {
 
 
 
-newAcc = (clamp(0, 0.5, newAcc));
+newAcc = (clamp(0.1, 0.5, newAcc));
 biquadFilter.frequency.value = biquadAcc;
 
 
