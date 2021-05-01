b987a43c4cea331472a20b23b16e76f885982d46
Mari L <lesteberg@gmail.com>
2021-05-01 13:50:49 +0200

    adding animation

--- prototype1.4.js
+++ prototype1.4.js
@@ -114,22 +114,11 @@ function handleOrientation(event) {
       pitchShift(Math.abs(event.gamma), synth3, diatonicScale); */
 
       // Animation code:
-          var id = null;
-      function myMove() {
+
       var elem = document.getElementById("myAnimation");   
-      var pos = 0;
-      clearInterval(id);
-      id = setInterval(frame, 10);
-      function frame() {
-        if (pos == 350) {
-          clearInterval(id);
-        } else {
-          pos++; 
           elem.style.top = event.alpha + 'px'; 
           elem.style.left = event.beta + 'px'; 
-        }
-      }
-      }
+
 
 
   }
