c382bec2a85934f9610c7ab5699da6d3fe4ff07a
Mari L <lesteberg@gmail.com>
2021-05-03 13:33:24 +0200

    Improving the QOM

--- prototype1.6.js
+++ prototype1.6.js
@@ -167,7 +167,7 @@ var i = 0;
     var elem = document.getElementById("myAnimation");   
     
 
-   var fn = generateScaleFunction(0.3, 1, 0.9, 0);
+   var fn = generateScaleFunction(0.3, 0.5, 0.9, 0);
     newAcc = fn(totAcc);
     newAcc = (clamp(0, 0.9, newAcc));
     //console.log(newAcc);
