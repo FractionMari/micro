9abced27977db612baa62bcb719d9b28a8785019
Mari L <lesteberg@gmail.com>
2021-05-11 10:21:09 +0200

    changing sensitivity of autoWah effect

--- prototype3.2.js
+++ prototype3.2.js
@@ -256,11 +256,11 @@ var i = 0;
        
        function loopActivate(players1, players2, value) {
        
-         if (pitchWheel < value)
+         if (yDotValues < value)
          players1.mute = true,
          players2.mute = true;
        
-         else if ((xDotValues > 50) && (pitchWheel > value))
+         else if ((xDotValues > 50) && (yDotValues > value))
          players2.mute = false,
          players1.mute = true;
        
@@ -270,10 +270,10 @@ var i = 0;
        
        };
        
-       loopActivate(player, player1_2, 2);
-       loopActivate(player2, player2_2, 4);
-       loopActivate(player3, player3_2, 6);
-       loopActivate(player4, player4_2, 8);
+       loopActivate(player, player1_2, 11);
+       loopActivate(player2, player2_2, 30);
+       loopActivate(player3, player3_2, 50);
+       loopActivate(player4, player4_2, 60);
        //loopActivate(player5, player5_2, 80);
        
        
